becbeb1a411703daf6c2deef08a8285419193c0a
SumanaMahesh <30820945+SumanaMahesh@users.noreply.github.com>
2018-04-18 09:59:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,26 +1,30 @@
 <?php
-	include 'config.php';
-	$myusername = $_POST['username'];
-	$mypassword = $_POST['password']; 
-	if(isset($_GET['sel'] && ! empty($_GET['sel']))
-	   {
-		   $username=$_GET['sel'];
-		   echo "candidate=".$username;
-	   }
-	   else echo ("Hello".$_GET
-	$query = "select * from login where username = \"$myusername\"" ;
-	$result = mysqli_query($dbh,$query) 
-		or die ('Error executing the query' .mysqli_error($dbh));
-	if($mysqli_num_rows($result)!= 1)
-		die("User doesn't exist");
-		$row=mysqli_fetch_array($result);
-	if($row['password']==$mypassword)
-	{
-		header("location: vote.html");
-		echo("Success!");
-	}
-	else
-	{	
-		header("location: login.html");
-	} 
+session_start();
+include '5418.php';
+	$myusername=$_POST['username'];
+	$mypassword=$_POST['password'];
+	
+if(isset($_GET['sel'] && ! empty($_GET['sel']))
+{
+$username=$_GET['sel'];
+echo "candidate=".$username;
+}
+else echo("hello".$_GET['sel']);
+ return;
+}
+
+$query="update vote=vote+1 where username="$myusername;".$_SESSION['sec'];
+//select * from candidate where section=."$_SESSION['sec']";
+	$result=mysqli_query($dbh,$query) or die ('error executing the query'.mysqli_error($dbh));
+if(mysqli_num_rows($result)!=1) 
+   die(" user does not exist");
+$row=mysqli_fetch_array($result);
+
+if( $row['password']==$mypassword)
+{
+ header('Location:ajaxp.html');
+ echo ("succcess");
+}	
+else
+ header('Location:login1.html');
 ?>
